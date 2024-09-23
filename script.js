8406288e364a9a79d12d4ab82bd98f6ea2ea0b11
NIKHITA PARDHE <153489800+Nikhita-55@users.noreply.github.com>
2024-09-23 17:07:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-//complete this code
-class Rectangle {}
+// Rectangle class
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
 
-class Square extends Animal {}
+    // Getter for width
+    get width() {
+        return this._width;
+    }
+
+    // Getter for height
+    get height() {
+        return this._height;
+    }
+
+    // Method to calculate area
+    getArea() {
+        return this._width * this._height;
+    }
+}
+
+// Square class that extends Rectangle
+class Square extends Rectangle {
+    constructor(sideLength) {
+        super(sideLength, sideLength); // Use the same value for width and height
+    }
+
+    // Method to calculate perimeter
+    getPerimeter() {
+        return 4 * this._width;
+    }
+}
+
+// Example usage:
+
+// Create a rectangle
+let rect = new Rectangle(4, 5);
+console.log(`Rectangle Area: ${rect.getArea()}`);  // Output: 20
+
+// Create a square
+let sq = new Square(4);
+console.log(`Square Area: ${sq.getArea()}`);       // Output: 16
+console.log(`Square Perimeter: ${sq.getPerimeter()}`);  // Output: 16
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
